2a99a5100734cd0afce1d69d1c7b28ed9b80a8f2
Greg Davis <gregjdavis1@gmail.com>
2022-02-18 20:02:08 -0500

    test

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,10 +1,13 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner kb = new Scanner(System.in);
+			
 	}
 
 }
